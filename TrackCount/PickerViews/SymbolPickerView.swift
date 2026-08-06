78d87b59509a3b2ce3e60499d00909d09d8b266e
Somebud0180 <ethanlagera@icloud.com>
2026-08-06 22:41:54 +0800

    fixed case of symbol set name

--- TrackCount/PickerViews/SymbolPickerView.swift
+++ TrackCount/PickerViews/SymbolPickerView.swift
@@ -26,7 +26,7 @@ struct SymbolPickerView: View {
         "objects": ["hammer.fill", "wrench.fill", "screwdriver.fill", "paintbrush.fill", "scissors", "pencil", "list.clipboard.fill", "archivebox.fill", "tray.2.fill", "bag.fill", "cart.fill", "gift.fill", "lightbulb.fill", "fanblades.fill", "microwave.fill", "oven.fill", "fork.knife", "cup.and.saucer.fill", "book.fill", "umbrella.fill", "balloon.fill", "party.popper.fill"],
         "math": ["plus", "minus", "plusminus", "multiply", "divide", "equal", "lessthan", "greaterthan", "pi", "number"],
         "transport": ["car.fill", "car.2.fill", "bolt.car.fill", "bus.fill", "bus.doubledecker.fill", "tram.fill", "airplane", "fuelpump.fill", "bicycle", "figure.walk", "location.fill", "location.north.fill", "mappin", "map.fill"],
-        "Time": ["clock.fill", "alarm.fill", "stopwatch.fill", "timer", "hourglass", "clock.fill", "deskclock.fill"],
+        "time": ["clock.fill", "alarm.fill", "stopwatch.fill", "timer", "hourglass", "clock.fill", "deskclock.fill"],
         "technology": ["laptopcomputer", "iphone", "ipad", "desktopcomputer", "applewatch", "watch.analog", "tv.fill", "printer.fill", "network", "antenna.radiowaves.left.and.right"],
         "people": ["person.fill", "person.2.fill", "person.circle", "person.crop.circle.badge.checkmark", "person.crop.circle.badge.xmark", "figure.wave", "figure.run", "figure.walk", "figure.stand", "eyes.inverse", "brain.head.profile", "ear", "hand.raised", "hand.wave"],
         "food": ["fork.knife", "cup.and.saucer.fill", "mug.fill", "wineglass.fill", "waterbottle.fill", "leaf.fill", "cart.fill", "takeoutbag.and.cup.and.straw.fill"],
